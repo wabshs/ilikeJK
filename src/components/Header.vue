648ff87bf54d43aa0adbbf627f107ed72330ec17
Nino <du840654613@163.com>
2024-02-01 18:36:25 +0800

    [add] 使header吸顶

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,32 @@
 <template>
   <div>
-
+    <el-menu
+        :default-active="activeIndex"
+        class="el-menu"
+        mode="horizontal"
+        :ellipsis="false"
+    >
+      <el-menu-item index="0">
+        <img src="/src/assets/vue.svg"
+             style="width: 100px"
+             alt="logo"
+        />
+      </el-menu-item>
+      <div class="flex-grow"/>
+      <el-menu-item index="1">Processing Center</el-menu-item>
+      <el-menu-item index="2">Processing Center</el-menu-item>
+    </el-menu>
   </div>
 </template>
 
 <script setup lang="ts">
+import {ref} from "vue";
+const activeIndex = ref('1')
 
 </script>
 
 <style scoped>
-
+.flex-grow {
+  flex-grow: 1;
+}
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,16 +1,36 @@
 <template>
   <div>
+    <el-container>
+      <el-affix>
+        <el-header class="header">
+          <Header/>
+        </el-header>
+      </el-affix>
 
+      <el-main style="min-height: 1500px">
+
+      </el-main>
+
+      <!--      页脚-->
+      <el-footer class="footer">
+        Smart School™ Designed By Du CopyRight 2024
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
 <script setup lang="ts">
 
 
-
+import Header from "../components/Header.vue";
 </script>
 
 <style scoped>
+.footer {
+  text-align: center;
+
+}
+
 
 
 </style>
