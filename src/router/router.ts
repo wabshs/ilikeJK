06f28e2f360cf6464bfafeb5a60c07aa05fe71cc
Neon <840654613@qq.com>
2024-03-19 11:26:52 +0800

    新建编辑 关于我 页面

--- src/router/router.ts
+++ src/router/router.ts
@@ -10,7 +10,8 @@ const routes: Array<RouteRecordRaw> = [
         redirect: 'index',
         children: [
             {path: 'index', name: 'Index', component: () => import('../views/pub/Welcome.vue')},
-            {path: 'my', name: 'My', component: () => import('../views/my/My.vue')}
+            {path: 'my', name: 'My', component: () => import('../views/my/My.vue')},
+            {path: 'edit', name: 'Edit', component: () => import('../views/my/EditAboutMe.vue')}
         ]
     }
 ]
